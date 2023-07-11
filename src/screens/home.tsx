890cfcd92545c54b582ca1fa5ad2fe0e2eb8dde9
Jamie Turner <Jamie.Turner@softwire.com>
2023-07-11 12:24:10 +0100

    Displays dropdown stations

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Text, Button } from 'react-native-paper';
+import { Text, Button, List } from 'react-native-paper';
 
 import { ScreenNavigationProps } from '../routes';
 
@@ -14,17 +14,36 @@ const styles = StyleSheet.create({
   text: {
     paddingBottom: 24,
   },
+  dropdown : {
+    width : '80%',
+    alignItems: 'center'
+  }
 });
 
 type HomeScreenProps = ScreenNavigationProps<'Home'>;
 
-const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Home Screen</Text>
+const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
+  const [location, setLocation] = React.useState("LDN");
+
+  const stations = ["SOU", "RYS", "OXF", "RDG", "WRW"]
+  return (<View style={styles.container}>
+    <List.Section style={styles.dropdown}>
+      <List.Accordion
+      title="Dropdown"
+      >
+        {
+          stations.map(station => {
+            return <List.Item title={station} onPress={()=>{setLocation(station)}}/>
+          })
+        }
+      </List.Accordion>
+    </List.Section>
+    <Text style={styles.text}>{location}</Text>
     <Button mode="contained" onPress={() => navigation.navigate('Details')}>
       Go to details
     </Button>
-  </View>
-);
+  </View>);
+}
+  
 
 export default HomeScreen;
